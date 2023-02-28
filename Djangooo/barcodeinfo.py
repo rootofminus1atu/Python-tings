ff2146435d8ace515249a10a190d365794456387
s00240122 <s00240122@atu.ie>
2023-02-28 18:15:31 +0000

    python fixing
    yeah

--- Djangooo/barcodeinfo.py
+++ Djangooo/barcodeinfo.py
@@ -2,19 +2,21 @@ import requests
 import json
 
 barcode = "5099195002161"
-url = f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json"
+url = f"https://world.openfoodfacts.org/api/v0/product/{barcode}?fields=packaging.json"
 response = requests.get(url)
 
+
+
+
+
 # printing info
-"""
 if response.status_code == 200:
     product_info = response.json()
     print(product_info)
-    # print("\n")
-    # print(product_info["product"]["ecoscore_data"]["adjustments"]["packaging"])
+    print("\n")
+    print(product_info["product"]["ecoscore_data"]["adjustments"]["packaging"])
 else:
     print(f"Error: {response.status_code}")
-"""
 
 # making a file with info
 if response.status_code == 200:
---
+++ Djangooo/funny.py
@@ -0,0 +1,1 @@
+# spam google sheets api
