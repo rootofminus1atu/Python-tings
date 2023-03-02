55fc19631b8ab44cda96fce0fafb80e13efac647
s00240122 <s00240122@atu.ie>
2023-03-02 15:10:46 +0000

    hi sam

--- Djangooo/barcode_improved_imporved.py
+++ Djangooo/barcode_improved_imporved.py
@@ -1,29 +1,30 @@
 import requests
 
 
-def error_thing():
-    print(f"Status code: {response.status_code}")
-    try:
-        print(f"{response.json()['status_verbose']}")
-    except:
-        pass
 
 
-barcode = "8076800195057"
-url = f"https://off:off@ie.openfoodfacts.net/api/v2/product/{barcode}"
-response = requests.get(url)
+def print_barcode_info(barcode):
+    def error_thing():
+        print(f"Status code: {response.status_code}")
+        try:
+            print(f"{response.json()['status_verbose']}")
+        except:
+            pass
 
-if response.status_code == 200:
-    data = response.json()
-    try:
-        packaging_info = data['product'].get('packaging')
-        packagings_info = data['product'].get('packagings')
-        print(data)
-        print("hi")
-        print(packaging_info)
-        print("hi")
-        print(packagings_info)
-    except:
+    url = f"https://off:off@ie.openfoodfacts.net/api/v2/product/{barcode}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        try:
+            packaging_info = data['product'].get('packaging')
+            packagings_info = data['product'].get('packagings')
+            print(data)
+            print("hi")
+            print(packaging_info)
+            print("hi")
+            print(packagings_info)
+        except:
+            error_thing()
+    else:
         error_thing()
-else:
-    error_thing()
---
+++ Djangooo/barcode_test.py
@@ -0,0 +1,7 @@
+from barcode_improved_imporved import print_barcode_info
+
+print_barcode_info("8076800195057")
+
+# execute script2 and pass barnum to it
+
+
