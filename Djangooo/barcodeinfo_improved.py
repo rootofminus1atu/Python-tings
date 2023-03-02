98c751de4e78cb05123704bafd890242b9988b0e
s00240122 <s00240122@atu.ie>
2023-03-02 13:50:49 +0000

    hi sam

--- Djangooo/barcodeinfo_improved.py
+++ Djangooo/barcodeinfo_improved.py
@@ -1,15 +1,26 @@
 import requests
 
-# ?fields=packaging
-# ?fields=packagings
+# ?fields=packagings,packaging
 
-barcode = "7622300735838"
-url = f"https://off:off@ie.openfoodfacts.net/api/v2/product/{barcode}?fields=packagings"
+barcode = "8076800195057"
+url = f"https://off:off@ie.openfoodfacts.net/api/v2/product/{barcode}?fields=packagings,packaging"
 response = requests.get(url)
 
+
+def error_thing():
+    print(f"Status code: {response.status_code}")
+    try:
+        print(f"{response.json()['status_verbose']}")
+    except:
+        pass
+
+
 if response.status_code == 200:
-    product_info = response.json()
-    print(product_info)
-    print(product_info['product']['packagings'])
+    data = response.json()
+    print(f"Prod info {data}")
+    try:
+        print(data['product']['packagings'])
+    except:
+        error_thing()
 else:
-    print(f"Error: {response.status_code}")
+    error_thing()
