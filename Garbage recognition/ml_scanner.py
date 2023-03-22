d51c289a91b56a8c8f70f8471980bb8653e50a2c
s00240122 <s00240122@atu.ie>
2023-03-22 09:17:45 +0000

    Update ml_scanner.py

--- Garbage recognition/ml_scanner.py
+++ Garbage recognition/ml_scanner.py
@@ -1,10 +1,39 @@
 import cv2
+import requests
+import os
 import ml_api
 import pynput
 import numpy as np
+from dotenv import load_dotenv
+load_dotenv()
 
-# cv2 opens a cam
-# we take a pic by pressing some key
-# that pic gets sent to the ml api to tell us what's it made of
+API_URL = "https://api-inference.huggingface.co/models/yangy50/garbage-classification"
+headers = {"Authorization": f"Bearer {os.getenv('API_URL')}"}
 
-# preferably without having to save and upload the pic, by for example using BytesIO (look it up on google)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    success, frame = cap.read()
+
+    cv2.imshow('frame', frame)
+
+    key = cv2.waitKey(1)
+    if key == ord(' '):
+        _, img_encoded = cv2.imencode('.jpg', frame)
+        response = requests.post(API_URL, headers=headers, data=img_encoded.tobytes())
+
+        if response.ok:
+            predictions = response.json()
+            print(predictions)
+            cv2.waitKey(10000)
+        else:
+            print(f"Request failed with status code {response.status_code}")
+        break
+
+    elif key == 27:  # 27 is the ASCII code for the Esc key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
