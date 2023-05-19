82ad044b10c7c7a9791f74aa3c8423d97472f98a
RootOfMinus1ATU <s00240122@atu.ie>
2023-05-19 16:55:36 +0100

    Update logger.py

--- Category theory/logger.py
+++ Category theory/logger.py
@@ -7,3 +7,25 @@ def negate(x):
 def square(x):
     msg = f"Squaring {x}"
     return x ** 2, msg
+
+
+class Logger:
+    def __init__(self, value, log=[]):
+        self.value = value
+        self.log = log
+        
+    def bind(self, f):
+        result, msg = f(self.value)
+        return Logger(result, self.log + [msg])
+    
+test_val = Logger(10).bind(negate).bind(square).bind(negate)
+print(test_val.value, test_val.log)
+
+result = (
+    Logger(2)
+    .bind(negate)
+    .bind(square)
+    .bind(negate)
+    .bind(square)
+)
+print(result.value, result.log)
