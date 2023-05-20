2191e2c5a1df16b02fed0894b241698dd34cb0da
RootOfMinus1ATU <s00240122@atu.ie>
2023-05-20 19:21:22 +0100

    Update linq_copy.py

--- Category theory/linq_copy.py
+++ Category theory/linq_copy.py
@@ -1,15 +1,6 @@
 from typing import Iterable
 from time import time, sleep
 
-class IterPlus:
-    def __init__(self, value: Iterable, log=[]):
-        self.value = value
-        self.log = log
-        
-    def bind(self, f):
-        result, msg = f(self.value)
-        return IterPlus(result, self.log + [msg])
-    
 
 def timer(f):
     def wrapper(*args, **kwargs):
@@ -43,7 +34,7 @@ class ListBetter:
     def filter(self, func):
         return self._create_instance(filter(func, self.value))
     
-
+"""
 import timeit
 
 lst = [x for x in range(10000)]
@@ -66,3 +57,54 @@ for i in [100, 500, 1000, 10000]:
     print(f"Time taken by built-in map with {i} tries:", time_builtin)
     print()
 
+"""
+
+
+class IterBetter:
+    def __init__(self, value: Iterable, log=[]):
+        self.value = value
+        self.log = log
+        self.original_type = type(value)
+        
+    def __repr__(self):
+        return f"{self.value}"
+    
+    def __getitem__(self, index):
+        return self.value[index]
+    
+    def __setitem__(self, index, value):
+        self.value[index] = value
+        
+    @staticmethod
+    def with_bind(f):
+        
+        def wrapper(self, func):
+            result, msg = f(self, func)
+            return self._create_instance(self.original_type(result), self.log + [msg])
+        
+        return wrapper
+    
+    def _create_instance(self, val):
+        return IterBetter(self.original_type(val), self.log)
+    
+    @with_bind
+    def map(self, func):
+        msg = f"Mapping {func} over {self.value}"
+        return self._create_instance(map(func, self.value)), msg
+    
+    @with_bind
+    def filter(self, func):
+        msg = f"Filtering {func} over {self.value}"
+        return self._create_instance(filter(func, self.value)), msg
+    
+    
+mylst = IterBetter([1,2,3,4,5])
+newlst = (
+    mylst
+    .map(lambda x: x + 1)
+    .map(lambda x: x + 1)
+    .map(lambda x: x + 1)
+)
+
+print(newlst.log)
+print(newlst)
