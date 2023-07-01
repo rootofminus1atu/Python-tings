eadd2216429e6d3bd88fb5211ce7ccb858852ef4
RootOfMinus1ATU <s00240122@atu.ie>
2023-07-01 23:14:50 +0100

    Update bot_dev_scheme.py

--- Maiq bot/bot_dev_scheme.py
+++ Maiq bot/bot_dev_scheme.py
@@ -2,6 +2,13 @@
 
 
 class BotDev:
-    def __init__(self, dev: bool, tester: bool):
+    def __init__(self, name, dev: bool, tester: bool):
+        self.name = name
         self.dev = dev
         self.tester = tester
+
+    @classmethod
+    def get_users(cls):
+        return [
+            cls("bud", dev=True, tester=True),
+        ]
