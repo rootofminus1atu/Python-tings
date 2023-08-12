0eeba8bb3c88644d26edd851dac409890aefe416
RootOfMinus1ATU <s00240122@atu.ie>
2023-08-12 16:39:57 +0100

    Update testing2.py

--- Bot base/testing2.py
+++ Bot base/testing2.py
@@ -74,6 +74,19 @@ class Interval:
         upper_check = value <= self.upper_num if self.upper_closed else value < self.upper_num
 
         return lower_check and upper_check
+    
+    def __eq__(self, other):
+        if isinstance(other, Interval):
+            return (
+                self.lower_closed == other.lower_closed and
+                self.lower_num == other.lower_num and
+                self.upper_num == other.upper_num and
+                self.upper_closed == other.upper_closed
+            )
+        return False
+
+    def __hash__(self):
+        return hash((self.lower_closed, self.lower_num, self.upper_num, self.upper_closed))
 
     def validate(self, field: str) -> None:
         real_num = r"-?\d*\.?\d+"
@@ -192,4 +205,63 @@ m2 = IntervalMap({
 print(m2)
 print(m2.slide(6))
 print(m2[5])
+
+
+
+class OverlappingIntervalsError(Exception):
+    pass
+
+class AnotherIntervalMap(dict):
+    def __init__(self, data: dict[Interval, any], not_found_case=None):
+        self.check_for_overlaps(data)
+        super().__init__(data)
+        self.not_found_case = not_found_case
+
+    def __repr__(self):
+        the_dict = {str(interval): value for interval, value in self.items()}
+        return f"<AnotherIntervalMap {the_dict}>"
+    
+    def slide(self, num: float) -> Any:
+        for interval, value in self.items():
+            if num in interval:
+                return value
+            
+        return self.not_found_case
+    
+    def check_for_overlaps(self, data: dict[Interval, any]) -> None:
+        """
+        A helper method that checks if there are any overlapping intervals in the data.
+
+        The current implementation works only with connected intervals, for example `(1, 2]` or `(-inf, -5)`.
+        """
+        intervals = list(data.keys())
+
+        for i in range(len(intervals)):
+            first = intervals[i]
+            second = intervals[i + 1] if i + 1 < len(intervals) else None
+
+            if second is None:
+                break
+
+            singular_overlap = first.upper_num == second.lower_num and (first.upper_closed and second.lower_closed)  # e.g. (1, 2] and [2, 3)
+            continuous_overlap = first.upper_num > second.lower_num  # e.g. (1, 3) and (2, 4)
+
+            if singular_overlap or continuous_overlap:
+                raise OverlappingIntervalsError(f"Intervals {str(first)} and {str(second)} overlap. Overlapping intervals are not allowed.")
+
+
+mp = {
+    Interval("(-inf, 1)"): "cold",
+    Interval("[1, 15)"): "medium",
+    Interval("[15, 30)"): "hot"
+}
+mp[Interval("[30, inf)")] = "very hot"
+
+m3 = AnotherIntervalMap(mp, not_found_case="not found")
+
+print(m3)
+
+m3.pop(Interval("[1, 15)"))
+
+print(m3)
 
