b6e8e2e99b527ec8954f7da19fb5b70e7773623d
RootOfMinus1ATU <s00240122@atu.ie>
2024-09-18 22:18:40 +0100

    testing clev

--- Cleverbot bot/cleverbot_final.py
+++ Cleverbot bot/cleverbot_final.py
@@ -33,6 +33,8 @@ class CleverbotConversation:
         payload += "cb_settings_scripting=no&islearning=1&icognoid=wsf&icognocheck="
         payload += hashlib.md5(payload[7:33].encode()).hexdigest()
 
+        print(payload)
+
         return payload
 
     def send_cleverbot_request(self, payload):
@@ -65,4 +67,14 @@ class CleverbotConversation:
 
 if __name__ == "__main__":
     conversation = CleverbotConversation()
+
+    conversation.context_queue.append("are you a bot !#$%&'()*+,/:;=?@[]~ \\\n\tetcEND")
+    conversation.context_queue.append("no !#$&'()*+,/:;=?@[]~ END")
+    conversation.context_queue.append("how are you")
+    conversation.context_queue.append("!#$%&'()*+,/:;=?@[]~\\\n\t\"<>^`{|}\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x7F")
+
+    p = conversation.build_payload("some stim :) !#$%&'()*+,/:;=?@[]~ END")
+    print(p)
+    
+
     conversation.start_conversation()
